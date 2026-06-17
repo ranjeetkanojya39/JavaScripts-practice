f8d1fa88108d5e512466e5debd7495b5e756f519
ranjeetkanojiya39 <ranjeetskanojiya20@gmail.com>
2026-06-17 17:12:01 +0530

    add one more topic

--- vudeo_no4/oops/callback/call.js
+++ vudeo_no4/oops/callback/call.js
@@ -1,11 +1,21 @@
-function kcuhDerBaadChalunga(callback){
-    setTimeout(
-        ()=>{
-            console.log(callback());
-        },
-        Math.floor(Math.random() * 7 )*1000
-    );
-} 
-kcuhDerBaadChalunga(function(){
-    console.log("I will run after some time");
+// function kcuhDerBaadChalunga(callback){
+//     setTimeout(
+//         ()=>{
+//             console.log(callback());
+//         },
+//         Math.floor(Math.random() * 7 )*1000
+//     );
+// } 
+// kcuhDerBaadChalunga(function(){
+//     console.log("I will run after some time");
+// });
+
+
+function profileLekeaao(username , cb){
+    setTimeout(() => {
+        console.log(`profile feched of ${username}`);
+    }, 2000);
+
+}
+profileLekeaao("sachin", function(){
 });
